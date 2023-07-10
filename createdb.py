1bb8dc4c72fbad8e73c0755fb1b7c9f90d48dec4
Sethro <93166687+S3THR0@users.noreply.github.com>
2023-07-10 03:50:58 -0400

    Revised createdb.py

--- createdb.py
+++ createdb.py
@@ -1,39 +1,37 @@
 import sqlite3
 import pyfiglet
 
+def main():
+    # Text in alligator font
+    out = pyfiglet.figlet_format("S3THR0", font="alligator")
+    print(out)
 
-#Text in alligator font
-out = pyfiglet.figlet_format("S3THR0", font="alligator")
-print(out)
+    # Connect to database
+    conn = sqlite3.connect("projectx.db")
 
-#connect to database
-conn = sqlite3.connect("projectx.db")
-        #sqlite3.connect(":memory:") for temp
-#create cursor
-c = conn.cursor()
-#create table
-c.execute("""CREATE TABLE members(
-        first_name text,
-        last_name text,
-        email text NOT NULL UNIQUE,
-        user_name text NOT NULL UNIQUE,
-        password text,
-        privilege integer
-    )""")
+    # Create cursor
+    c = conn.cursor()
 
-#5 datatypes, NULL, INTEGER, REAL, TEXT, BLOB
-"""
-NULL - DOESNT EXIST
-INTEGER - WHOLE NUMBERS
-REAL - DECIMAL NUMBERS
-TEXT - TEXT
-BLOB - IMAGES, MP3 ETC
-"""
+    # Create table
+    c.execute("""
+        CREATE TABLE members(
+            first_name text,
+            last_name text,
+            email text NOT NULL UNIQUE,
+            user_name text NOT NULL UNIQUE,
+            password text,
+            privilege integer
+        )
+    """)
 
-#commit command/save our progress
-conn.commit()
-#close connection
-conn.close()
+    # Commit command/save our progress
+    conn.commit()
 
-print("Created database Successfully")
-k=input("hit enter to exit:")
+    # Close connection
+    conn.close()
+
+    print("Created database Successfully")
+    input("Press enter to exit:")
+
+if __name__ == "__main__":
+    main()
