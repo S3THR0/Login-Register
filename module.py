0a010a10763114ef40ce0f45b5ee24d6a39927f1
Sethro <93166687+S3THR0@users.noreply.github.com>
2023-07-10 03:47:04 -0400

    Revised module.py

--- module.py
+++ module.py
@@ -2,126 +2,74 @@ import sqlite3
 import maskpass
 import socket
 
+DATABASE = "projectx.db"
 
-conn = sqlite3.connect("projectx.db")
-#sqlite3.connect(":memory:") for temp
-#create cursor
-c = conn.cursor()
+def connect_to_db():
+    return sqlite3.connect(DATABASE)
 
-def Roster():
-    #connect to database
-    conn = sqlite3.connect("projectx.db")
-    #sqlite3.connect(":memory:") for temp
-    # #create cursor
+def disconnect_from_db(conn):
+    conn.commit()
+    conn.close()
+
+def Roster(conn):
     c = conn.cursor()
-    #query database
     c.execute("SELECT * FROM members")
-    # #print(c.fetchone()[3])
-    # #print(c.fetchmany())
-    # #print(c.fetchall())
-    #organized fetch
     datas = c.fetchall()
-    print("NAME " + "\t\t\tUSERNAME " + "\tEMAIL")
-    print("---------" + "\t\t---------" + "\t--------")
+    print("NAME \t\t\tUSERNAME \tEMAIL")
+    print("---------\t\t---------\t--------")
     for data in datas:
-        print(data[0] + " " + data[1] + "\t\t" + data[3] + "\t\t" + data[2] )
-    #commit command/save our progress
-    conn.commit()
-    # #close connection
-    conn.close()
+        print(f'{data[0]} {data[1]}\t\t{data[3]}\t\t{data[2]}')
 
-
-def Credentials():
+def Credentials(conn):
     print("Credential Check")
-    #connect to database
-    conn = sqlite3.connect("projectx.db")
-            #sqlite3.connect(":memory:") for temp
-    #create cursor
     c = conn.cursor()
-    statement = "SELECT user_name, password FROM members"
-    c.execute(statement)
-    #organized fetch
+    c.execute("SELECT user_name, password FROM members")
     datas = c.fetchall()
-    print("USERNAME " + "\tPASSWORD")
-    print("---------" + "\t--------")
+    print("USERNAME \tPASSWORD")
+    print("---------\t--------")
     for data in datas:
-            print(data[0] + "\t\t" + data[1])
+        print(f'{data[0]}\t\t{data[1]}')
+    input("press enter to exit")
 
-
-    k=input("press enter to exit")
-    #commit command/save our progress
-    conn.commit()
-    #close connection
-    conn.close()
-
-def Register():
-    #connect to database
-    conn = sqlite3.connect("projectx.db")
-            #sqlite3.connect(":memory:") for temp
-    #create cursor
+def Register(conn):
     c = conn.cursor()
-
     firstname = input("What is your first name?:")
     lastname = input("What is your last name?:")
     email = input("What is your email?:")
     emailconfirm = input("Confirm your email:")
 
-    if (email == emailconfirm):
-        username = input("Create Username:")
-        if username == "root":
-            privilege = 1
-        else:
-            privilege = 0
+    if email != emailconfirm:
+        print("Emails do not match!!")
+        return
 
-        password = maskpass.askpass(mask="")
-        member_info = [
-                        (firstname, lastname, email, username, password, privilege)
-                    ]
+    username = input("Create Username:")
+    privilege = 1 if username == "root" else 0
+    password = maskpass.askpass(mask="")
 
-        c.executemany("INSERT INTO members VALUES (?,?,?,?,?,?)", member_info)
-        print("Successfully Registered " + username)
-        #commit command/save our progress
-        conn.commit()
-        # #close connection
-        conn.close()
-    else:
-        print("Please review email!!")
+    member_info = [(firstname, lastname, email, username, password, privilege)]
+    c.executemany("INSERT INTO members VALUES (?,?,?,?,?,?)", member_info)
+    print("Successfully Registered " + username)
 
-
-def RRegister():
-    #connect to database
-    conn = sqlite3.connect("projectx.db")
-            #sqlite3.connect(":memory:") for temp
-    #create cursor
+def RRegister(conn):
     c = conn.cursor()
-
     firstname = input("What is your first name?:")
     lastname = input("What is your last name?:")
     email = input("What is your email?:")
     emailconfirm = input("Confirm your email:")
 
-    if (email == emailconfirm):
-        username = input("Create Username:")
-        if username == "root":
-            privilege = 1
-        else:
-            privilege = 0
+    if email != emailconfirm:
+        print("Emails do not match!!")
+        return
 
-        password = maskpass.askpass(mask="")
-        print("0 = non root, 1 = root")
-        priveledge =input("Priveledge (0 or 1)")
-        member_info = [
-                        (firstname, lastname, email, username, password, privilege)
-                    ]
+    username = input("Create Username:")
+    privilege = 1 if username == "root" else 0
+    password = maskpass.askpass(mask="")
+    print("0 = non root, 1 = root")
+    privilege = input("Priveledge (0 or 1)")
 
-        c.executemany("INSERT INTO members VALUES (?,?,?,?,?,?)", member_info)
-        print("Successfully Registered " + username)
-        #commit command/save our progress
-        conn.commit()
-        # #close connection
-        conn.close()
-    else:
-        print("Please review email!!")
+    member_info = [(firstname, lastname, email, username, password, privilege)]
+    c.executemany("INSERT INTO members VALUES (?,?,?,?,?,?)", member_info)
+    print("Successfully Registered " + username)
 
 def Login():
     username = input("Username:")
@@ -129,61 +77,37 @@ def Login():
     hostname = socket.gethostname()
     IPAddr = socket.gethostbyname(hostname)
 
-    #connect to database
-    conn = sqlite3.connect("projectx.db")
-            #sqlite3.connect(":memory:") for temp
-    #create cursor
+    conn = connect_to_db()
     c = conn.cursor()
 
-    statement = f"SELECT user_name from members WHERE user_name = '{username}' AND password = '{password}';"
-    c.execute(statement)
-    if not c.fetchone():  # An empty result evaluates to False.
+    statement = f"SELECT user_name from members WHERE user_name = ? AND password = ?;"
+    c.execute(statement, (username, password))
+    if not c.fetchone():
         print("Login failed")
+        disconnect_from_db(conn)
         exit()
-    else:
-        print("Welcome " + username)
-        print("Your Computer Name is: " + hostname)
-        print("Your Computer local IP Address is: " + IPAddr)
 
-    RPcheck = f"SELECT user_name FROM members WHERE user_name = '{username}' AND privilege = '1'"
-    c.execute(RPcheck)
+    print("Welcome " + username)
+    print("Your Computer Name is: " + hostname)
+    print("Your Computer local IP Address is: " + IPAddr)
+
+    RPcheck = f"SELECT user_name FROM members WHERE user_name = ? AND privilege = '1'"
+    c.execute(RPcheck, (username,))
     if not c.fetchone():
         print("You are not admin")
+        disconnect_from_db(conn)
         exit()
-    else:
-        pass
 
-    menu = {}
-    menu['1']="Roster" 
-    menu['2']="Credentials"
-    menu['3']="Register User"
-    menu['7']="Exit"
+    menu = {'1': Roster, '2': Credentials, '3': RRegister, '7': "Exit"}
     while True:
-        options = menu.keys()
-        for entry in options:
-            print(entry, menu[entry])
-        
+        for entry, action in menu.items():
+            print(entry, action.__name__ if callable(action) else action)
         selection = input("Please Select:")
-        if selection =='1': 
-            Roster()
-        elif selection == '2':
-            Credentials()
-        elif selection == '3':
-            RRegister()
-        elif selection == '7':
-            break
+        if selection in menu:
+            if callable(menu[selection]):
+                menu[selection](conn)
+            else:
+                break
         else:
             print("Unknown Option Selected!")
-    #commit command/save our progress
-    conn.commit()
-    # #close connection
-    conn.close()
-
-
-
-
-
-#commit command/save our progress
-    conn.commit()
-    # #close connection
-    conn.close()
+    disconnect_from_db(conn)
